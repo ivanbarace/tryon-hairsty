f99cea44dfca6641ebcbca080de95a1119222bcb
ivanbarace <185926776+ivanbarace@users.noreply.github.com>
2025-03-14 22:52:56 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/tryon-hairsty/',
   plugins: [
     react(),
     VitePWA({
@@ -18,8 +19,8 @@ export default defineConfig({
         theme_color: '#ffffff',
         background_color: '#ffffff',
         display: 'standalone',
-        start_url: '/',
-        scope: '/',
+        start_url: '/tryon-hairsty/',
+        scope: '/tryon-hairsty/',
         icons: [
           {
             src: '/icon-192x192.png',
