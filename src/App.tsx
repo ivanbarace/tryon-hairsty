5a87fdbdcbb87bf54f6523c9869add3323e192bc
ivanbarace <185926776+ivanbarace@users.noreply.github.com>
2025-03-14 23:01:30 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import './App.css'
 import Login from './Components/Login/Login'
 import AdminDashboard from './Components/AdminDashboard/AdminDashboard'
@@ -9,27 +9,25 @@ import ProtectedRoute from './Components/ProtectedRoute/ProtectedRoute'
 
 function App() {
   return (
-    <BrowserRouter basename="/tryon-hairsty">
-      <div className="app-container">
-        <main>
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/forgot-password" element={<ForgotPassword />} />
-            <Route
-              path="/admin-dashboard/*"
-              element={
-                <ProtectedRoute>
-                  <AdminDashboard />
-                </ProtectedRoute>
-              }
-            />
-            <Route path="/user/*" element={<User />} />
-            <Route path="/" element={<Navigate to="/user/home" replace />} />
-          </Routes>
-        </main>
-      </div>
-    </BrowserRouter>
+    <div className="app-container">
+      <main>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route
+            path="/admin-dashboard/*"
+            element={
+              <ProtectedRoute>
+                <AdminDashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/user/*" element={<User />} />
+          <Route path="/" element={<Navigate to="/user" replace />} />
+        </Routes>
+      </main>
+    </div>
   )
 }
 
